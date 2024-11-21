b3f862b8c7e751e19587024688131d900cfe3da6
Keysia keys <keysia.keys0207@gmail.com>
2024-11-21 13:36:36 +0700

    Perubahan Kedelapan

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+
+function validateForm() {
+    const nameInput = document.getElementById('name').value;
+    console.log(nameInput) 
+}
+
+let indexSlide = 1;
+const listImages = document.getElementsByClassName ('banner-autoslide');
+
+function nextSlide() {
+    showSlide(indexSlide += 1);
+}
+
+function showSlide(index) {
+   // if (index > listImages.length) indexSlide = 1;
+   console.log(indexSlide)
+   hideAllSlide();
+   listImages[indexSlide - 1].style.display = 'block';
+}
+// Logic untuk menyembunyikan semua slide
+function hideAllSlide() {
+    for (let i = 0; i < listImages.length; i++) {
+    listImages[i].style.display = 'none';
+    }
+}
